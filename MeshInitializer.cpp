7a4300ab5783bfbe5c6c720324248408526734c1
guillaumetousignant <moi_guillaume@hotmail.com>
2018-10-14 18:26:21 -0400

    nfaces

--- MeshInitializer.cpp
+++ MeshInitializer.cpp
@@ -19,8 +19,11 @@ void MeshInitializer::initializeMesh(string& meshFilename){
     // pre-pass should give the following results
     unsigned int npoints = 16512;
     unsigned int ncells = 16384;
-    unsigned int nghost = 128 + 128; // far field and airfoil
-    unsigned int nface = 100000; // calculate this
+    unsigned int nghosts = 128 + 128; // far field and airfoil
+    unsigned int nfaces = 100000; // calculate this
+
+
+
 
 }
 
