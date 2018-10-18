c99fe2263f37a4ac42fe93f4692a834be09aef31
frbenb <43252241+frbenb@users.noreply.github.com>
2018-10-18 17:08:21 -0400

    some comments were removed.

--- Mesh_Data.h
+++ Mesh_Data.h
@@ -15,11 +15,11 @@ class Mesh_Data{
   
     public:
     //Input
-    double* Nodes_x_;               //Done
-    double* Nodes_y_;               //Done
+    double* Nodes_x_;               
+    double* Nodes_y_;               
 
     //Connectivity
-    unsigned int** Cell2Node_;               //Done
+    unsigned int** Cell2Node_;               
     unsigned int** Cell2Face_;
     unsigned int** Face2Node_;
     unsigned int** Face2Cell_;
@@ -27,14 +27,14 @@ class Mesh_Data{
     unsigned int** Node2Cell_;
 
     //Helper
-    unsigned int* CellNfaces_;               //Done
-    unsigned int NCells_;           //Done
-    unsigned int NCellsGhost_;      //lui
-    unsigned int NCellsTotal_;      // ?
-    unsigned int NFaces_;           //Done
-    unsigned int NFacesGhost_;      //Done // should not exist I think
-    unsigned int NFacesTotal_;      //Done // should not exist I think
-    unsigned int NNodes_;           //Done
+    unsigned int* CellNfaces_;     
+    unsigned int NCells_;        
+    unsigned int NCellsGhost_;     
+    unsigned int NCellsTotal_;      
+    unsigned int NFaces_;           
+    unsigned int NFacesGhost_;      // should not exist I think
+    unsigned int NFacesTotal_;      // should not exist I think
+    unsigned int NNodes_;           
     double* Volume_;
     double* Residu_;
 
