cc2a74ae28da686b720f76e9ba2763460f6dc92b
guillaumetousignant <moi_guillaume@hotmail.com>
2018-10-19 12:47:20 -0400

    New dummy functions for deallocate

--- Mesh_Data.cpp
+++ Mesh_Data.cpp
@@ -16,22 +16,22 @@ rho_(nullptr), u_(nullptr), v_(nullptr), p_(nullptr){}
 /************************************DESTRUCTOR BY DEFAULT****************************************/
 Mesh_Data::~Mesh_Data()
 {
-    Nodes_x_ = deallocate1D(Nodes_x_);
-    Nodes_y_ = deallocate1D(Nodes_y_);
-    CellNfaces_ = deallocate1D(CellNfaces_);
-    Volume_ = deallocate1D(Volume_);
-    Residu_ = deallocate1D(Residu_);
-    rho_ = deallocate1D(rho_);
-    u_ = deallocate1D(u_);
-    v_ = deallocate1D(v_);
-    p_ = deallocate1D(p_);
+    Nodes_x_ = deallocate1Ddbl(Nodes_x_);
+    Nodes_y_ = deallocate1Ddbl(Nodes_y_);
+    CellNfaces_ = deallocate1Dint(CellNfaces_);
+    Volume_ = deallocate1Ddbl(Volume_);
+    Residu_ = deallocate1Ddbl(Residu_);
+    rho_ = deallocate1Ddbl(rho_);
+    u_ = deallocate1Ddbl(u_);
+    v_ = deallocate1Ddbl(v_);
+    p_ = deallocate1Ddbl(p_);
 
-    Cell2Node_ = deallocate2D(Cell2Node_, NCellsTotal_);
-    Cell2Face_ = deallocate2D(Cell2Face_, NCellsTotal_);
-    Face2Node_ = deallocate2D(Face2Node_, NFaces_);
-    Face2Cell_ = deallocate2D(Face2Cell_, NFaces_);
-    Cell2Cell_ = deallocate2D(Cell2Cell_, NCellsTotal_);
-    Node2Cell_ = deallocate2D(Node2Cell_, NNodes_);
+    Cell2Node_ = deallocate2Dint(Cell2Node_, NCellsTotal_);
+    Cell2Face_ = deallocate2Dint(Cell2Face_, NCellsTotal_);
+    Face2Node_ = deallocate2Dint(Face2Node_, NFaces_);
+    Face2Cell_ = deallocate2Dint(Face2Cell_, NFaces_);
+    Cell2Cell_ = deallocate2Dint(Cell2Cell_, NCellsTotal_);
+    Node2Cell_ = deallocate2Dint(Node2Cell_, NNodes_);
 
     NCells_ = 0;
     NCellsGhost_ = 0;
