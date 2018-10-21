192f5b7ace75b23eb47ac011895d2318630b50ea
patrick.deschambault <patrick.deschambault@polymtl.ca>
2018-10-21 15:02:37 -0400

    attributes of nscData

--- NSCData.h
+++ NSCData.h
@@ -21,18 +21,48 @@ class NSCData
         //Data.
         std::string ctrlfilename_;
         std::string meshfilename_;
+        std::string title_;
+        int topo_;
 
         double gamma_;
         double alpha_;
         double mach_;
+        double reynolds_;
+        double tinf_;
+
+        /* flow & geometry properties */
+	    double xref_;
+        double yref_; //from "input file"
+        double cmac_;
+
         double pi_;
+        double epsilon_;
 
         double rhoInfini_;
         double uInfini_;
         double vInfini_;
         double pInfini_;
 
-        double epsilon_;
+        double vis2_;
+        double vis4_;
+        double ressmo_;
+
+        unsigned int nitc_, niter_;
+        double* itccfl_; //iterate timestep
+        double cfl_;
+
+        unsigned int dissip_;
+        unsigned int itertot_;
+        
+        double* rk_alfa_;
+        double* rk_beta_;
+        
+        /* constants from "input file" */
+        unsigned int nbiter_;
+    
+        /*convergence*/
+        double rms0_;
+
 
 };
 
