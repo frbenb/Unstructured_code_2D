c83a9e4731f92fbee59af9721d193135e444e5e1
patrick.deschambault <patrick.deschambault@polymtl.ca>
2018-10-21 14:12:43 -0400

    Add MainSolver Class

---
+++ MainSolver.cpp
@@ -0,0 +1,17 @@
+
+#include "MainSolver.h"
+
+
+MainSolver::MainSolver(NSCData *iNSCData, Mesh_Data *iMeshData) :
+                                    _nscData(iNSCData),
+                                    _meshData(iMeshData)
+{
+
+}
+
+MainSolver::~MainSolver()
+{
+    
+}
+
+
