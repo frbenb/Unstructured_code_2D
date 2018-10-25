9d07f8f1642de959e4c701acee25ee4d0c4c2585
patrick.deschambault <patrick.deschambault@polymtl.ca>
2018-10-25 10:42:08 -0400

    Implementation of void methods for solver

--- MainSolver.cpp
+++ MainSolver.cpp
@@ -3,15 +3,76 @@
 
 
 MainSolver::MainSolver(NSCData *iNSCData, Mesh_Data *iMeshData) :
-                                    _nscData(iNSCData),
-                                    _meshData(iMeshData)
+                                    nscData_(iNSCData),
+                                    meshData_(iMeshData)
 {
-
+    
 }
 
 MainSolver::~MainSolver()
 {
     
 }
+
+void MainSolver::doUpdate()
+{
+    //Inside a loop, stop when convergence...
+
+        //1. Iterate_pseudo_timestep
+
+        //2. Monitor the convergence.
+
+}
+
+void MainSolver::computeSolution()
+{
+
+    //In a loop, for nstage...
+
+        //1. spectral_radius
+
+        //2. residual
+
+        //3. residual smoothing
+
+        //4. Update solution
+
+        //5. Update boundary
+
+}
+
+void MainSolver::timestep()
+{
+
+}
+
+void MainSolver::saveW0()
+{
+
+
+}
+
+void MainSolver::spectral_radius()
+{
+
+
+}
+
+void MainSolver::residual()
+{
+
+}
+
+void MainSolver::residual_smoothing()
+{
+
+}
+
+void MainSolver::monitor_convergence()
+{
+
+
+}
+
 
 
